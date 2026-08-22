e82e4a8b22a27ab26a8e20a94068a2a17fb73e44
Git practice <jagadishb0000@gmail.com>
2026-08-22 13:34:00 +0530

    initialing search fearure by employeeId

--- Employee.java
+++ Employee.java
@@ -3,11 +3,16 @@ class Employee{
     String name;
     double salary;
     String role ;
+
+    public void searchbyEmployeeId(){
+        System.out.println("This is search by employee id method");
+    }
 }
 class Main{
     public static void main(String[] arg){
         System.out.println("This is employee java file");
         System.out.println("This is small change file");
-
     }
+
+
 }
