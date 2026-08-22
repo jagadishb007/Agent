9d23e68fd3b12dc959af9ad5f1445e7a6e5a5284
Git practice <jagadishb0000@gmail.com>
2026-08-22 12:48:19 +0530

    updated employee java file adding roles

---
+++ Employee.java
@@ -0,0 +1,12 @@
+class Employee{
+    int id;
+    String name;
+    double salary;
+    String role ;
+}
+class Main{
+    public static void main(String[] arg){
+        System.out.println("This is employee java file");
+
+    }
+}
